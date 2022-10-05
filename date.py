136cd76a6b43cad6cf32f497ab36c28e68abe0d3
Enrique Perez <eperezarrua@gmail.com>
2022-10-05 11:53:22 -0300

    tercer Commit

---
+++ date.py
@@ -0,0 +1,3 @@
+import datetime
+my_time = datetime.datetime.now()
+print(my_time)
---
+++ set2.py
@@ -0,0 +1,3 @@
+random_list = [1, 2, 2, 2, 3, "Platzi", "Platzi", True, 6, 6, False]
+myset = set(random_list)
+print(myset)
